ffc93b301e0daecfa42396aa433a4398d7a152d1
vaporii <phi548182@gmail.com>
2025-04-17 20:50:52 -0500

    add those dependencies

--- nix/default.nix
+++ nix/default.nix
@@ -4,9 +4,19 @@ pkgs.buildGoModule {
   pname = "v8p.me-cli";
   version = "0.1.0";
 
+  nativeBuildInputs = [ pkgs.makeWrapper ];
+
   src = ../.;
 
   vendorHash = "sha256-un4+9Pozo8ArUP6m0BIoeidpDrksxte0WZrfDJH9Mwo=";
+
+  postFixup = ''
+    wrapProgram $out/bin/v8p.me-cli \
+      --set PATH ${pkgs.lib.makeBinPath (with pkgs; [
+        xclip
+        wl-clipboard
+    ])}
+  '';
 
   # installPhase = ''
   #   mkdir -p $out/bin
