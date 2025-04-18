49606edfe425ea3c1e8387210c4beefda16687b1
vaporii <phi548182@gmail.com>
2025-04-17 20:36:43 -0500

    shell alias

--- nix/home-manager.nix
+++ nix/home-manager.nix
@@ -1,11 +1,13 @@
 inputs: { config, lib, pkgs, ... }: let
   inherit (pkgs.stdenv.hostPlatform) system;
+  package = inputs.self.packages.${system}.default;
 in {
   options.programs.v8p = {
     enable = lib.mkEnableOption "v8p";
   };
 
   config = lib.mkIf config.programs.v8p.enable {
-    home.packages = [ inputs.self.packages.${system}.default ];
+    home.packages = [ package ];
+    home.shellAliases.v8p = "${package}/bin/v8p.me-cli";
   };
 }
